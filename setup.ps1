371f45ee6d0d4e79fd4b22d8fe04bb555c2e16ab
Shaun Wong <ChronoXP@hotmail.com>
2019-09-28 22:43:07 +0800

    Add installs to setup

--- setup.ps1
+++ setup.ps1
@@ -19,3 +19,79 @@ else
   Write-Host "Install Chocolatey"
   Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
+
+# Browsers
+choco install -y googlechrome firefox
+choco install -y adblockplus-firefox
+
+# Comms
+choco install -y discord slack
+
+# Media
+choco install -y spotify vlc
+
+# Apps
+choco install -y qbittorrent
+choco install -y vscode
+choco install -y todolist
+
+# Utils
+choco install -y 7zip 
+choco install -y teamviewer
+choco install -y teracopy
+choco install -y imgburn
+choco install -y adobereader
+choco install -y lightshot
+choco install -y handbrake
+choco install -y treesizefree
+
+# Network
+choco install -y wireshark
+choco install -y angryip
+choco install -y putty
+
+# Runtimes
+choco install -y silverlight
+choco install -y adobeair
+
+# Dev
+choco install -y hyper
+choco install -y git.install
+choco install -y nodejs.install
+choco install -y yarn
+choco install -y epicgameslauncher
+
+# VS Code Plugins
+refreshenv
+# TODO Fetch from Mac-setup repo instead
+echo "Installing vscode themes and plugins"
+code --install-extension monokai.theme-monokai-pro-vscode
+code --install-extension alexanderte.dainty-vscode
+code --install-extension dracula-theme.theme-dracula
+code --install-extension pkief.material-icon-theme
+## Extensions
+code --install-extension alefragnani.project-manager
+code --install-extension aliariff.auto-add-brackets
+code --install-extension apollographql.vscode-apollo
+code --install-extension burkeholland.react-food-truck
+code --install-extension burkeholland.simple-react-snippets
+code --install-extension capaj.vscode-exports-autocomplete
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension christian-kohler.path-intellisense
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension eamodio.gitlens
+code --install-extension eg2.vscode-npm-script
+code --install-extension eriklynd.json-tools
+code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension kumar-harsh.graphql-for-vscode
+code --install-extension mgmcdermott.vscode-language-babel
+code --install-extension Prisma.vscode-graphql
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension wix.vscode-import-cost
+code --install-extension lunarlimbo.banner-comments-plus
+code --install-extension idered.figma
